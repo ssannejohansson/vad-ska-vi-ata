a713c3465076ae2e1e0b1d25466c23d3d445ced9
ssannejohansson <sannej0hansson@hotmail.com>
2025-09-03 18:57:06 +0200

    Put everything on one page

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,7 @@ function shuffleArray(arr) {
     .map(({ value }) => value);
 }
 
-$("#generate-food").on("click", randomDish);
-
-
-let dishDisplayContent = '<div class="container d-flex flex-column justify-content-center align-items-center text-center" id="dish-container">' + 
-'<h1 class="mb-5" id="dish"></h1>' +
-'<p class="m-auto">Vill du ha lite ny inspiration?</p>' +
-'<a class="mt-2 btn custom-btn-link mb-5" id="link" target="_blank">Testa detta receptet!</a>' +
-'<p class="m-auto">Inte sugen?</p>' +
-'<button class="btn custom-btn mt-2" id="shuffle">Generera igen</button>' +
-'</div>';
-
+$("#shuffle").on("click", randomDish);
 
 function randomDish() {
 if (currentFoodIndex >= shuffledFoodList.length) { 
@@ -38,16 +28,10 @@ if (currentFoodIndex >= shuffledFoodList.length) {
   img = randomDish.img;
 
 
-
 $("#dish").text(dish);
 $("a").attr("href", link);
 $("img").attr("src", img);
-$("#shuffle").on("click", dishDisplay);
 }
 
-function dishDisplay() {
-    $(".container").children().remove();
-   $("#glass-container").append(dishDisplayContent);
-   randomDish();
-}
+randomDish();
 
